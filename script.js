a5732aa3ced30c454e765a16adbcfd6f000ad1cd
Ehssan-B <ehssanbaghban@outlook.com>
2025-06-16 14:57:38 +0100

    complete pseudocode

---
+++ script.js
@@ -0,0 +1,20 @@
+// Etch a sketch pseudocode
+
+/*
+
+Goal: 
+    1. Create (or recreate) a grid of squares (max size: 100x100)
+    2. Allow user to colour squares by hovering
+    3. Clear grid
+
+function createGrid (gridSize):
+    create container div
+    for i 0 to gridSize:
+        create square div
+        add classname = i
+        add eventListener:
+            change class of hovered square to active to change colour
+        add square div to container div
+
+*/
+
